e16a6857964576eb0bb01401ac27ecc8f90d065c
Lazaros Toumanidis <laztoum@waldiez.io>
2025-09-25 17:34:29 +0300

    update port utils tests

--- tests/dependencies/test_utils.py
+++ tests/dependencies/test_utils.py
@@ -19,28 +19,12 @@ def test_is_port_available_free_port() -> None:
 
 def test_is_port_available_used_port() -> None:
     """Test if is_port_available returns False for a port in use."""
-    # occupy a port
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
-        soc.bind(("", 0))
-        soc.listen(1)
-        used_port = soc.getsockname()[1]
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        sock.bind(("", 0))
+        _, port = sock.getsockname()
 
-        # While the socket is open, the port should not be available
-        assert is_port_available(used_port) is False
-
-    # After closing, the port should be available again
-    assert is_port_available(used_port) is True
-
-
-def test_get_available_port() -> None:
-    """Test if get_available_port returns a port that is actually available."""
-    port = get_available_port()
-
-    # Ensure the returned port is actually available
-    assert is_port_available(port) is True
-
-    # occupy the port
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
-        soc.bind(("localhost", port))
-        soc.listen(1)
+        # Port should not be available
         assert is_port_available(port) is False
+    finally:
+        sock.close()
