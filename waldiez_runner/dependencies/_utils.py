b173a9e36547d71ab4ebdb23a1cc4a9a4e48145e
Lazaros Toumanidis <laztoum@waldiez.io>
2025-09-25 17:03:37 +0300

    update port utils

--- waldiez_runner/dependencies/_utils.py
+++ waldiez_runner/dependencies/_utils.py
@@ -3,24 +3,42 @@
 """Utility functions for the dependencies module."""
 
 import socket
+from contextlib import closing
 
 
+# noinspection PyBroadException
 def is_port_available(port: int) -> bool:
-    """Check if a port is available.
+    """Check if the port is available.
 
     Parameters
     ----------
     port : int
-        Port to check.
+        Port number
 
     Returns
     -------
     bool
-        True if the port is available, False otherwise.
+        True if port is available
     """
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
-        result = soc.connect_ex(("localhost", port))
-        return result != 0
+    # Check IPv4
+    try:
+        with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+            sock.bind(("", port))
+    except BaseException:  # pylint: disable=broad-exception-caught
+        return False
+
+    # Check IPv6
+    try:  # pragma: no cover
+        with closing(
+            socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+        ) as sock:
+            # Disable dual-stack to only check IPv6
+            sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 1)
+            sock.bind(("", port))
+    except BaseException:  # pylint: disable=broad-exception-caught
+        return False
+
+    return True
 
 
 def get_available_port() -> int:
@@ -29,9 +47,9 @@ def get_available_port() -> int:
     Returns
     -------
     int
-        The available port.
+        An available port number
     """
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
-        soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as soc:
         soc.bind(("", 0))
+        soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         return soc.getsockname()[1]
